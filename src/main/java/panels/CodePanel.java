89ab293c2f8aa5ec1b1bb5c0c779f234afcbb600
diego-rascon <joserascon2002@gmail.com>
2023-03-20 19:37:41 -0700

    Se implementó la carga del archivo .xlsx en CodePanel.java

--- src/main/java/panels/CodePanel.java
+++ src/main/java/panels/CodePanel.java
@@ -1,15 +1,23 @@
 package panels;
 
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.fife.ui.rsyntaxtextarea.RSyntaxTextArea;
 import org.fife.ui.rsyntaxtextarea.SyntaxConstants;
 import org.fife.ui.rsyntaxtextarea.Theme;
 import org.fife.ui.rtextarea.RTextScrollPane;
 
+import javax.swing.*;
 import java.awt.*;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class CodePanel extends PanelTemplate {
 
+    private int[][] matrix;
     private final RSyntaxTextArea codeArea = new RSyntaxTextArea();
 
     public CodePanel() {
@@ -23,31 +31,60 @@ public class CodePanel extends PanelTemplate {
         codeArea.setAntiAliasingEnabled(true);
 
         try {
-            Theme theme = Theme.load(getClass().getResourceAsStream(
-                    "/org/fife/ui/rsyntaxtextarea/themes/monokai.xml"));
+            Theme theme = Theme.load(getClass().getResourceAsStream("/org/fife/ui/rsyntaxtextarea/themes/monokai.xml"));
             theme.apply(codeArea);
         } catch (IOException ioe) { // Never happens
             ioe.printStackTrace();
         }
 
         add(scrollPane, BorderLayout.CENTER);
-
-//        final Color white = new Color(255, 255, 255);
-//
-//        codeArea.setBackground(new Color(40, 40, 40));
-//        codeArea.setForeground(white);
-//        codeArea.setSelectedTextColor(white);
-//        codeArea.setCurrentLineHighlightColor(new Color(66, 66, 66));
-//        codeArea.setSelectionColor(new Color(0, 122, 255));
-//        codeArea.setMarginLineColor(Color.red);
+        loadMatrix();
     }
 
     public void compile() {
         final String code = codeArea.getText();
-        int state = 0;
+        //int state = 0;
         for (int i = 0; i < code.length(); i++) {
             final char character = code.charAt(i);
             System.out.println(character);
+            System.out.println(Arrays.deepToString(matrix));
+        }
+    }
+
+    final void loadMatrix() {
+        try {
+            FileInputStream matrixFile = new FileInputStream("./src/main/resources/matrix.xlsx");
+
+            XSSFWorkbook workbook = new XSSFWorkbook(matrixFile);
+            XSSFSheet sheet = workbook.getSheetAt(0);
+
+            int rowCount = sheet.getLastRowNum();
+
+            for (int i = 1; i < rowCount; i++) {
+                XSSFRow row = sheet.getRow(i);
+                int columnCount = row.getLastCellNum();
+
+                matrix = new int[rowCount - 1][columnCount - 1];
+
+                for (int j = 1; j < columnCount; j++) {
+                    Cell cell = row.getCell(j);
+                    int cellValue = (int) cell.getNumericCellValue();
+                    matrix[i - 1][j - 1] = cellValue;
+                }
+            }
+        } catch (IOException e) {
+            final Object[] options = {"OK"};
+            final int selection = JOptionPane.showOptionDialog(
+                    JOptionPane.getFrameForComponent(this),
+                    "El archivo .xlsx de la matríz no pudo ser encontrado.",
+                    "Error",
+                    JOptionPane.OK_CANCEL_OPTION,
+                    JOptionPane.ERROR_MESSAGE,
+                    null,
+                    options,
+                    options[0]
+            );
+            if (selection == JOptionPane.OK_OPTION) System.exit(0);
         }
     }
 
