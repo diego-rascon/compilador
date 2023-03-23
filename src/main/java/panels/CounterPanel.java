c96fd429195dfa4221baf23152318b6468541d79
diego-rascon <joserascon2002@gmail.com>
2023-03-22 23:17:26 -0700

    Se avanzó con el funcionamiento de los contadores

--- src/main/java/panels/CounterPanel.java
+++ src/main/java/panels/CounterPanel.java
@@ -2,12 +2,35 @@ package panels;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.Map;
 
 public class CounterPanel extends PanelTemplate {
 
-    private final HashMap<String, Integer> counterMap = new HashMap<String, Integer>();
+    private final LinkedHashMap<String, Integer> counterMap = new LinkedHashMap<>();
+    private final String[] rowLabels = {
+            "Operadores de postfix",
+            "Operadores lógicos binarios",
+            "Operadores de control",
+            "Operadores matemáticos",
+            "Operadores de exponente",
+            "Operadores de turno",
+            "Operadores relacionales",
+            "Operadores sin igualdad",
+            "Operadores lógicos",
+            "Operadores ternarios",
+            "Operadores de asignación",
+            "Operadores de agrupamiento",
+            "Palabras reservadas",
+            "Comentarios",
+            "Constantes de cadena",
+            "Constantes numéricas",
+            "Constantes reales",
+            "Constantes booleanas",
+            "Constantes nulas",
+            "Identificadores",
+            "Errores"
+    };
     private final String[] columns = {"Tipo", "Cantidad"};
     private final DefaultTableModel counterTableModel = new DefaultTableModel(columns, 0);
 
@@ -16,7 +39,7 @@ public class CounterPanel extends PanelTemplate {
 
         final JTable counterTable = new JTable(counterTableModel);
         counterTable.getTableHeader().setReorderingAllowed(false);
-        
+
         final JScrollPane scrollPane = new JScrollPane(counterTable);
         add(scrollPane);
 
@@ -24,33 +47,86 @@ public class CounterPanel extends PanelTemplate {
     }
 
     final void initTable() {
-        counterMap.put("Operadores de postfix", 0);
-        counterMap.put("Operadores lógicos binarios", 0);
-        counterMap.put("Operadores de control", 0);
-        counterMap.put("Operadores matemáticos", 0);
-        counterMap.put("Operadores de exponente", 0);
-        counterMap.put("Operadores de turno", 0);
-        counterMap.put("Operadores relacionales", 0);
-        counterMap.put("Operadores sin igualdad", 0);
-        counterMap.put("Operadores lógicos", 0);
-        counterMap.put("Operadores ternarios", 0);
-        counterMap.put("Operadores de asignación", 0);
-        counterMap.put("Operadores de agrupamiento", 0);
-        counterMap.put("Palabras reservadas", 0);
-        counterMap.put("Comentarios", 0);
-        counterMap.put("Constantes de cadena", 0);
-        counterMap.put("Constantes numéricas", 0);
-        counterMap.put("Constantes reales", 0);
-        counterMap.put("Constantes booleanas", 0);
-        counterMap.put("Constantes nulas", 0);
-        counterMap.put("Identificadores", 0);
-        counterMap.put("Errores", 0);
+        for (String rowLabel : rowLabels) {
+            counterMap.put(rowLabel, 0);
+            final Object[] counterRow = {rowLabel, 0};
+            counterTableModel.addRow(counterRow);
+        }
+    }
 
+    public void restartCounter() {
         for (Map.Entry<String, Integer> counterEntry : counterMap.entrySet()) {
-            final String label = counterEntry.getKey();
-            final Integer value = counterEntry.getValue();
-            final Object[] counter = {label, value};
-            counterTableModel.addRow(counter);
+            counterEntry.setValue(0);
+        }
+    }
+
+    public void addCounter(int state) {
+        switch (state) {
+            case -2, -5 -> {
+                String rowLabel = rowLabels[0];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -7, -8, -10, -13 -> {
+                String rowLabel = rowLabels[1];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -15, -16, -17, -18 -> {
+                String rowLabel = rowLabels[2];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -1, -4, -19, -22, -26 -> {
+                String rowLabel = rowLabels[3];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -20 -> {
+                String rowLabel = rowLabels[4];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -29, -33, -34 -> {
+                String rowLabel = rowLabels[5];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -28, -32, -31, -37, -39, -43 -> {
+                String rowLabel = rowLabels[6];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -40, -44 -> {
+                String rowLabel = rowLabels[7];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -42, -11, -9 -> {
+                String rowLabel = rowLabels[8];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -45 -> {
+                String rowLabel = rowLabels[9];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -38, -3, -6, -21, -24, -27, -12, -14, -30, -36, -35, -41 -> {
+                String rowLabel = rowLabels[10];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case -46, -47, -48, -49, -50, -51 -> {
+                String rowLabel = rowLabels[11];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
+            case 500, 501, 502, 503, 504, 505 -> {
+                String rowLabel = rowLabels[20];
+                int newValue = counterMap.get(rowLabel) + 1;
+                counterMap.put(rowLabel, newValue);
+            }
         }
     }
 
