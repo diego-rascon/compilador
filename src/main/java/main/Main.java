1b9db759acc93bdec4525bef648e3b494f335f79
DiegoRascon <joserascon2002@gmail.com>
2023-04-17 17:24:39 -0700

    Se establece el ícono de la aplicación

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -7,6 +7,7 @@ public class Main {
     public static void main(String[] args) {
         final MainFrame mainFrame = new MainFrame();
 
+        mainFrame.setIconImage(new ImageIcon("./src/main/resources/code.png").getImage());
         mainFrame.setSize(800, 600);
         mainFrame.setLocationRelativeTo(null);
         mainFrame.setTitle("Léxico - 20130375");
