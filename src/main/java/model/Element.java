813a0803c75d2bf73b88ee0bc38bd294f9ef24a0
diego-rascon <joserascon2002@gmail.com>
2023-08-29 23:46:07 -0700

    Clase para almacenar los elementos de la base de datos

---
+++ src/main/java/model/Element.java
@@ -0,0 +1,85 @@
+package model;
+
+public class Element {
+
+    private final String name;
+    private String type;
+    private final String classType;
+    private final int ambit;
+    private int arraySize;
+    private int arrayDim;
+    private int parQuantity;
+    private String parType;
+
+    public Element(String name, String classType, int ambit) {
+        this.name = name;
+        this.classType = classType;
+        this.ambit = ambit;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getClassType() {
+        return classType;
+    }
+
+    public int getAmbit() {
+        return ambit;
+    }
+
+    public int getArraySize() {
+        return arraySize;
+    }
+
+    public void setArraySize(int arraySize) {
+        this.arraySize = arraySize;
+    }
+
+    public int getArrayDim() {
+        return arrayDim;
+    }
+
+    public void setArrayDim(int arrayDim) {
+        this.arrayDim = arrayDim;
+    }
+
+    public int getParQuantity() {
+        return parQuantity;
+    }
+
+    public void setParQuantity(int parQuantity) {
+        this.parQuantity = parQuantity;
+    }
+
+    public String getParType() {
+        return parType;
+    }
+
+    public void setParType(String parType) {
+        this.parType = parType;
+    }
+
+    @Override
+    public String toString() {
+        return "Element{" +
+                "name='" + name + '\'' +
+                ", type='" + type + '\'' +
+                ", classType='" + classType + '\'' +
+                ", ambit=" + ambit +
+                ", arraySize=" + arraySize +
+                ", arrayDim=" + arrayDim +
+                ", parQuantity=" + parQuantity +
+                ", parType='" + parType + '\'' +
+                '}';
+    }
+}
