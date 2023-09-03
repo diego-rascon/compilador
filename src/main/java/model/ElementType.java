a67796e308f54a75d1daa1ad6c669b58fb994333
diego-rascon <joserascon2002@gmail.com>
2023-09-02 22:24:53 -0700

    Se agregaron más estados

--- src/main/java/model/ElementType.java
+++ src/main/java/model/ElementType.java
@@ -2,7 +2,16 @@ package model;
 
 public enum ElementType {
     NONE,
-    DECVAR,
-    DECMET,
-    DECFUN,
+    DEC_VAR,
+    DEC_MET,
+    DEC_FUN,
+    DEC_CLASS,
+    DEC_INTER,
+    DEC_GET,
+    DEC_SET,
+    DEC_LET,
+    DEC_AN_MET,
+    DEC_AN_FUN,
+    DEC_AN_CLASS,
+    DEC_ARRAY
 }
