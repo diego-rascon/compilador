4476da5d600b28eac5aab59e3e970b685a44a051
Diego Rascón <joserascon2002@gmail.com>
2023-10-08 14:39:56 -0700

    Funciones para aumentar los contadores

--- src/main/java/model/Operation.java
+++ src/main/java/model/Operation.java
@@ -1,12 +1,29 @@
 package model;
 
 public class Operation {
+
     private final int line;
     private int strings, numbers, booleans, reals, customs, voids, variants, errors;
     private String assignation;
 
     public Operation(int line) {
         this.line = line;
+    }
+
+    public void addCounter(Type type) {
+        switch (type) {
+            case STRING -> strings++;
+            case NUMBER -> numbers++;
+            case BOOLEAN -> booleans++;
+            case REAL -> reals++;
+            case CUSTOM -> customs++;
+            case VOID -> voids++;
+            case VARIANT -> variants++;
+        }
+    }
+
+    public void addError() {
+        errors++;
     }
 
     public int getLine() {
@@ -17,64 +34,32 @@ public class Operation {
         return strings;
     }
 
-    public void setStrings(int strings) {
-        this.strings = strings;
-    }
-
     public int getNumbers() {
         return numbers;
-    }
-
-    public void setNumbers(int numbers) {
-        this.numbers = numbers;
     }
 
     public int getBooleans() {
         return booleans;
     }
 
-    public void setBooleans(int booleans) {
-        this.booleans = booleans;
-    }
-
     public int getReals() {
         return reals;
-    }
-
-    public void setReals(int reals) {
-        this.reals = reals;
     }
 
     public int getCustoms() {
         return customs;
     }
 
-    public void setCustoms(int customs) {
-        this.customs = customs;
-    }
-
     public int getVoids() {
         return voids;
-    }
-
-    public void setVoids(int voids) {
-        this.voids = voids;
     }
 
     public int getVariants() {
         return variants;
     }
 
-    public void setVariants(int variants) {
-        this.variants = variants;
-    }
-
     public int getErrors() {
         return errors;
-    }
-
-    public void setErrors(int errors) {
-        this.errors = errors;
     }
 
     public String getAssignation() {
