c9fc244e5c6f0d72b454d4171acc0f41b2ec504f
Diego Rascón <joserascon2002@gmail.com>
2023-10-08 22:28:49 -0700

    Se exporta la hoja de semántica

--- src/main/java/util/FileHandler.java
+++ src/main/java/util/FileHandler.java
@@ -1,6 +1,7 @@
 package util;
 
 import model.Ambit;
+import model.Operation;
 import org.apache.poi.xssf.usermodel.XSSFCell;
 import org.apache.poi.xssf.usermodel.XSSFRow;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
@@ -28,6 +29,7 @@ public class FileHandler {
     private final ErrorTypes errorTypesPanel;
     private final JFileChooser fileChooser = new JFileChooser();
     private final String[] ambitHeaders = {"Ámbito", "string", "number", "boolean", "real", "null", "#id", "void", "errores", "total"};
+    private final String[] semHeaders = {"Linea", "Strings", "Numbers", "Booleans", "Reales", "Variables #", "Voids", "Variants", "Asignaciones", "Errores"};
 
     public FileHandler(JFrame mainFrame, Code codePanel, Tokens tokenPanel, Counters countersPanel, Errors errorsPanel, ErrorTypes errorTypesPanel) {
         this.mainFrame = mainFrame;
@@ -121,77 +123,151 @@ public class FileHandler {
                 }
             }
 
-            final XSSFSheet sheet = workbook.createSheet("Ámbitos");
+            final XSSFSheet ambSheet = workbook.createSheet("Ámbitos");
             int rowNum = 0;
-            final XSSFRow sheetRow = sheet.createRow(rowNum++);
+            XSSFRow sheetRow = ambSheet.createRow(rowNum++);
             for (int i = 0; i < ambitHeaders.length; i++) {
                 final XSSFCell sheetCell = sheetRow.createCell(i);
                 sheetCell.setCellValue(ambitHeaders[i]);
             }
 
-            int[] totals = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+            int[] ambTotals = {0, 0, 0, 0, 0, 0, 0, 0, 0};
 
             LinkedList<Ambit> ambits = codePanel.getAmbits();
             for (Ambit ambit : ambits) {
                 var column = 0;
-                final XSSFRow row = sheet.createRow(rowNum++);
+                final XSSFRow row = ambSheet.createRow(rowNum++);
 
                 final XSSFCell ambitCell = row.createCell(column++);
                 ambitCell.setCellValue(ambit.getId());
 
                 final XSSFCell stringCell = row.createCell(column++);
                 int tempStrings = ambit.getStrings();
-                totals[0] += tempStrings;
+                ambTotals[0] += tempStrings;
                 stringCell.setCellValue(tempStrings);
 
                 final XSSFCell numberCell = row.createCell(column++);
                 int tempNumbers = ambit.getNumbers();
-                totals[1] += tempNumbers;
+                ambTotals[1] += tempNumbers;
                 numberCell.setCellValue(tempNumbers);
 
                 final XSSFCell boolCell = row.createCell(column++);
                 int tempBooleans = ambit.getBooleans();
-                totals[2] += tempBooleans;
+                ambTotals[2] += tempBooleans;
                 boolCell.setCellValue(tempBooleans);
 
                 final XSSFCell realCell = row.createCell(column++);
                 int tempReals = ambit.getReals();
-                totals[3] += tempReals;
+                ambTotals[3] += tempReals;
                 realCell.setCellValue(tempReals);
 
                 final XSSFCell nullCell = row.createCell(column++);
                 int tempNulls = ambit.getNulls();
-                totals[4] += tempNulls;
+                ambTotals[4] += tempNulls;
                 nullCell.setCellValue(tempNulls);
 
                 final XSSFCell customCell = row.createCell(column++);
                 int tempCustoms = ambit.getCustoms();
-                totals[5] += tempCustoms;
+                ambTotals[5] += tempCustoms;
                 customCell.setCellValue(tempCustoms);
 
                 final XSSFCell voidCell = row.createCell(column++);
                 int tempVoids = ambit.getVoids();
-                totals[6] += tempVoids;
+                ambTotals[6] += tempVoids;
                 voidCell.setCellValue(tempVoids);
 
                 final XSSFCell errorCell = row.createCell(column++);
                 int tempErrors = ambit.getErrors();
-                totals[7] += tempErrors;
+                ambTotals[7] += tempErrors;
                 errorCell.setCellValue(tempErrors);
 
                 final XSSFCell totalCell = row.createCell(column);
                 int tempTotals = ambit.getTotal();
-                totals[8] += tempTotals;
+                ambTotals[8] += tempTotals;
                 totalCell.setCellValue(tempTotals);
             }
 
             int totalsColumns = 0;
-            final XSSFRow totalsRow = sheet.createRow(rowNum);
-            final XSSFCell labelCell = totalsRow.createCell(totalsColumns++);
-            labelCell.setCellValue("total");
+            final XSSFRow ambTotalsRow = ambSheet.createRow(rowNum);
+            final XSSFCell ambLabelCell = ambTotalsRow.createCell(totalsColumns++);
+            ambLabelCell.setCellValue("total");
 
-            for (int total : totals) {
-                final XSSFCell totalCell = totalsRow.createCell(totalsColumns++);
+            for (int total : ambTotals) {
+                final XSSFCell totalCell = ambTotalsRow.createCell(totalsColumns++);
+                totalCell.setCellValue(total);
+            }
+
+            final XSSFSheet semSheet = workbook.createSheet("Semántica");
+            rowNum = 0;
+            sheetRow = semSheet.createRow(rowNum++);
+            for (int i = 0; i < semHeaders.length; i++) {
+                final XSSFCell sheetCell = sheetRow.createCell(i);
+                sheetCell.setCellValue(semHeaders[i]);
+            }
+
+            int[] semTotals = {0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+            LinkedList<Operation> operations = codePanel.getOperations();
+            for (Operation operation : operations) {
+                int columns = 0;
+                final XSSFRow row = semSheet.createRow(rowNum++);
+
+                final XSSFCell lineCell = row.createCell(columns++);
+                lineCell.setCellValue(operation.getLine());
+
+                final XSSFCell stringsCell = row.createCell(columns++);
+                int tempStrings = operation.getStrings();
+                semTotals[0] += tempStrings;
+                stringsCell.setCellValue(tempStrings);
+
+                final XSSFCell numbersCell = row.createCell(columns++);
+                int tempNumbers = operation.getNumbers();
+                semTotals[1] += tempNumbers;
+                numbersCell.setCellValue(tempNumbers);
+
+                final XSSFCell booleansCell = row.createCell(columns++);
+                int tempBooleans = operation.getBooleans();
+                semTotals[2] += tempBooleans;
+                booleansCell.setCellValue(tempBooleans);
+
+                final XSSFCell realsCell = row.createCell(columns++);
+                int tempReals = operation.getReals();
+                semTotals[3] += tempReals;
+                realsCell.setCellValue(tempReals);
+
+                final XSSFCell customsCell = row.createCell(columns++);
+                int tempCustoms = operation.getCustoms();
+                semTotals[4] += tempCustoms;
+                customsCell.setCellValue(tempCustoms);
+
+                final XSSFCell voidsCell = row.createCell(columns++);
+                int tempVoids = operation.getVoids();
+                semTotals[5] += tempVoids;
+                voidsCell.setCellValue(tempVoids);
+
+                final XSSFCell variantsCell = row.createCell(columns++);
+                int tempVariants = operation.getVariants();
+                semTotals[6] += tempVariants;
+                variantsCell.setCellValue(tempVariants);
+
+                final XSSFCell assignationsCell = row.createCell(columns++);
+                String tempAssignation = operation.getAssignation();
+                semTotals[7]++;
+                assignationsCell.setCellValue(tempAssignation);
+
+                final XSSFCell errorsCell = row.createCell(columns);
+                int tempErrors = operation.getErrors();
+                semTotals[8] += tempErrors;
+                errorsCell.setCellValue(tempErrors);
+            }
+
+            totalsColumns = 0;
+            final XSSFRow semTotalsRow = semSheet.createRow(rowNum);
+            final XSSFCell semLabelCell = semTotalsRow.createCell(totalsColumns++);
+            semLabelCell.setCellValue("Totales");
+
+            for (int total : semTotals) {
+                final XSSFCell totalCell = semTotalsRow.createCell(totalsColumns++);
                 totalCell.setCellValue(total);
             }
 
