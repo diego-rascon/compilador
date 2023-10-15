5546c91e43b9fc1dfa3228d608061099cf42f9bf
Diego Rascón <joserascon2002@gmail.com>
2023-10-14 22:34:20 -0700

    Se agregaron las funciones set y get

--- src/main/java/model/Semantics.java
+++ src/main/java/model/Semantics.java
@@ -5,5 +5,67 @@ public class Semantics {
     int rule, line, ambit;
     boolean accepted;
 
+    public Semantics(int rule) {
+        this.rule = rule;
+    }
 
+    public String getTopStack() {
+        return topStack;
+    }
+
+    public void setTopStack(String topStack) {
+        this.topStack = topStack;
+    }
+
+    public String getRealValue() {
+        return realValue;
+    }
+
+    public void setRealValue(String realValue) {
+        this.realValue = realValue;
+    }
+
+    public int getRule() {
+        return rule;
+    }
+
+    public void setRule(int rule) {
+        this.rule = rule;
+    }
+
+    public int getLine() {
+        return line;
+    }
+
+    public void setLine(int line) {
+        this.line = line;
+    }
+
+    public int getAmbit() {
+        return ambit;
+    }
+
+    public void setAmbit(int ambit) {
+        this.ambit = ambit;
+    }
+
+    public boolean isAccepted() {
+        return accepted;
+    }
+
+    public void setAccepted(boolean accepted) {
+        this.accepted = accepted;
+    }
+
+    @Override
+    public String toString() {
+        return "Semantics{" +
+                "topStack='" + topStack + '\'' +
+                ", realValue='" + realValue + '\'' +
+                ", rule=" + rule +
+                ", line=" + line +
+                ", ambit=" + ambit +
+                ", accepted=" + accepted +
+                '}';
+    }
 }
